3fe22b34de9e711f91132bb2838a9983acbc3169
Victor Koech <vickoch20@gmail.com>
2024-08-10 10:49:28 +0300

    Update function displayPosts

--- frontend-public/app.js
+++ frontend-public/app.js
@@ -14,16 +14,21 @@ function displayPosts(posts) {
     const container = document.getElementById('posts-container');
     container.innerHTML = '';
 
-    posts.forEach(post => {
+    posts.forEach((post, index) => {
         const postElement = document.createElement('article');
         postElement.classList.add('post');
+        postElement.setAttribute('data-aos', 'fade-up');
+        postElement.setAttribute('data-aos-delay', index * 100);
         postElement.innerHTML = `
             <h2>${post.title}</h2>
-            <p>${post.content}</p>
-            <small>By ${post.author.username} on ${new Date(post.createdAt).toLocaleDateString()}</small>
+            <div class="post-meta">By ${post.author.username} on ${new Date(post.createdAt).toLocaleDateString()}</div>
+            <p>${post.content.substring(0, 150)}...</p>
+            <a href="#" class="read-more">Read more</a>
         `;
         container.appendChild(postElement);
     });
+
+    AOS.init();
 }
 
 fetchPosts();
